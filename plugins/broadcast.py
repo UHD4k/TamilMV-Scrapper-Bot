bb6d9769d43c8ac07039918fbc9320db3b5d1bc7
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-06 18:52:24 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,16 +1,16 @@
 from pyrogram import Client ,filters
 import os
 from helper.database import getid
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+ADMIN = int(os.environ.get("ADMIN", 1391556668))
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
+   ms = await message.reply_text("**Geting All IDs From Database...**")
    ids = getid()
    tot = len(ids)
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   await ms.edit(f"**Completed Broadcast 💌\n Sending Message To {tot} Users**")
    for id in ids:
      try:
      	await message.reply_to_message.copy(id)
