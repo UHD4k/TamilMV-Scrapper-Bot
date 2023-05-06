4e535810ba165e7f85a9dd3e24243bcaa183c35f
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-06 19:17:19 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 async def inline(_, query: InlineQuery):
         string = query.query.lower()
         if string == "":
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("Ex ; ```@Googletranslateitbot how to use # hi```"),description="{Text} # {language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code}",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
         else:
         	splitit = string.split("#")        	     	
@@ -20,7 +20,7 @@ async def inline(_, query: InlineQuery):
         		cd = splitit[1].lower().replace(" ", "")
         		text = splitit[0]
         except:
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("Ex ; ```@Googletranslateitbot how to use # hi```"),description="{Text}#{language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
         	return
         try:
@@ -28,7 +28,7 @@ async def inline(_, query: InlineQuery):
         	translation = translator.translate(text,dest = cd)
         except:
         	return
-        TRTEXT=  [ InlineQueryResultArticle( title=f"Translated from {translation.src} To {translation.dest}", input_message_content=InputTextMessageContent(translation.text),description=f'{translation.text}',thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        TRTEXT=  [ InlineQueryResultArticle( title=f"Translated From {translation.src} To {translation.dest}", input_message_content=InputTextMessageContent(translation.text),description=f'{translation.text}',thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         try:
         	await query.answer(results=TRTEXT ,  cache_time=2, switch_pm_text="Google Translater",switch_pm_parameter="start")
         except:
