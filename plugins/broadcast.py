c725402c8ba7148520298cb4c7dc394d084608b4
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-07 20:04:39 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -28,7 +28,7 @@ async def stats(bot, message):
      	pass
 
 @Client.on_message(filters.command('users') & filters.private & filters.user(ADMIN))
-async def get_users(client, message: Message):
+async def get_users(client, message):
     msg = await client.send_message(chat_id=message.chat.id, text=f"**Processing**")
     users = await full_userbase()
     await msg.edit(f"**{len(users)} users are using this bot**")
