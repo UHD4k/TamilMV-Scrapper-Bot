c5ff8f7988bb99e05e75bd5ea78547cd285eb411
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-07 20:51:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ API_ID = int(os.environ.get("API_ID",12345))
 
 API_HASH = os.environ.get("API_HASH", "")
 
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001822021062"))
+
 if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
