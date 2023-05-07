d8b3bb3e124ebe344e683de11464f2f4e75305cc
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-07 11:37:20 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 async def inline(_, query: InlineQuery):
         string = query.query.lower()
         if string == "":
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code}",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        	METHOD=  [ InlineQueryResultArticle( title="Enter Your Text", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code}",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
         else:
         	splitit = string.split("#")        	     	
