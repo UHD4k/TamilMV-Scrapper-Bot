497edf2ba59c1372d020a3a65e8533d2660b5cb6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-07 12:17:45 +0530

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup
-    InlineQuery,
+
     InlineQueryResultArticle,
     InputTextMessageContent
 )
