b8b5ef83a86ed44909e76984ac491d027c9bd573
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-07 20:55:43 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,5 +1,8 @@
 import pymongo 
 import os
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
 
 DB_NAME = os.environ.get("DB_NAME","")
 DB_URL = os.environ.get("DB_URL","")
@@ -46,4 +49,19 @@ def full_userbase():
         user_ids.append(doc['_id'])
         
     return user_ids
-    
+ 
+def is_subscribed(filter, client, update):
+    if not FORCE_SUB_CHANNEL:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMIN:
+        return True
+    try:
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
+    except UserNotParticipant:
+        return False
+
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    else:
+        return True
