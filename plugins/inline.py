aab6c0a1e7cca3033e5f0b92105d21f30a59f1c4
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-09 07:02:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -20,7 +20,7 @@ async def inline(_, query: InlineQuery):
         		cd = splitit[1].lower().replace(" ", "")
         		text = splitit[0]
         except:
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        	METHOD=  [ InlineQueryResultArticle( title="Enter Your Text", input_message_content=InputTextMessageContent("**Example :-** ```@Google_Translator_Star_Bot How are You # Tamil```"),description="{Text} # {language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
         	return
         try:
@@ -33,3 +33,11 @@ async def inline(_, query: InlineQuery):
         	await query.answer(results=TRTEXT ,  cache_time=2, switch_pm_text="Google Translater",switch_pm_parameter="start")
         except:
         	return
+
+def get_reply_markup(query):
+    buttons = [
+        [
+            InlineKeyboardButton('Translate Again', switch_inline_query_current_chat=query)
+        ]
+        ]
+    return InlineKeyboardMarkup(buttons)
