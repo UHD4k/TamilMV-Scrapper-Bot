d4059ef2c36c2c2f8d74f0aed580e353feb16899
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-28 22:48:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,58 @@
-from pyrogram import Client
-import os
+from pyrogram import Client, __version__
+import re, os, time
+from datetime import datetime
+from pytz import timezone
+from pyrogram.raw.all import layer
+from config import Config
+from aiohttp import web
+from route import web_server
 
-TOKEN = os.environ.get("TOKEN", "6043919342:AAGVl9ktA1YDKYvVKu-6xycVX7-9Fg69ZbU")
-
+BOT_TOKEN = os.environ.get("TOKEN", "6043919342:AAGVl9ktA1YDKYvVKu-6xycVX7-9Fg69ZbU")
 API_ID = int(os.environ.get("API_ID",11973721))
-
 API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
+BOT_UPTIME  = time.time()
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 
-FORCE_SUB   = os.environ.get("FORCE_SUB", "Star_Bots_Tamil") 
+class Bot(Client):
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
-    app = Client(
-        "GTBot",
-        bot_token=TOKEN,
-        api_hash=API_HASH,
-        api_id=API_ID,
-        plugins=plugins
-    )
-    app.run()
+    def __init__(self):
+        super().__init__(
+            name="GTStarBot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username  
+        self.uptime = BOT_UPTIME     
+        if WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()       
+            await web.TCPSite(app, "0.0.0.0", 8080).start()     
+        print(f"{me.first_name} is Started...✨️\nMade By :- https://t.me/Star_Bots_Tamil")
+        for id in ADMIN:
+            try: await self.send_message(id, f"**__{me.first_name} is Started...✨️\nMade By :- [Star Bots Tamil](https://t.me/Star_Bots_Tamil)__**")                                
+            except: pass
+        if LOG_CHANNEL:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(LOG_CHANNEL, f"**__{me.mention} is Restarted. !!**\n\n📅 Date :- `{date}`\n⏰ Time :- `{time}`\n🌐 TimeZone :- `Asia/Kolkata`\n\n🉐 Version :- `v{__version__} (Layer {layer})`</b>")                                
+            except:
+                print("Please Make This is Admin in Your Log Channel")
+
+    async def stop(self, *args):
+        await super().stop()      
+        print("Bot Stopped... Bye")
+       
+
+Bot().run()
