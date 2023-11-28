889fde98bf4696a36466ea9eb2b65a09b4e9b19d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-28 22:33:39 +0530

    Update and rename imagetotext.py to Force_Sub.py

---
+++ plugins/Force_Sub.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters, enums 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
+from config import Config
+from helper.database import db
+
+async def not_subscribed(_, client, message):
+    await db.add_user(client, message)
+    if not Config.FORCE_SUB:
+        return False
+    try:             
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
+        if user.status == enums.ChatMemberStatus.BANNED:
+            return True 
+        else:
+            return False                
+    except UserNotParticipant:
+        pass
+    return True
+
+
+@Client.on_message(filters.private & filters.create(not_subscribed))
+async def forces_sub(client, message):
+    mention = message.from_user.mention
+    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    text = "**Hello {mention} 💗,\nJoin Our Bot Updates Channel To Use Me ☺️\nYou Need to Join Our Channel to Use me\nKindly Please Join Our Channel**"
+    try:
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
+        if user.status == enums.ChatMemberStatus.BANNED:                                   
+            return await client.send_message(message.from_user.id, text="**Sorry You're Banned to Use Me**")  
+    except UserNotParticipant:                       
+        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+          
