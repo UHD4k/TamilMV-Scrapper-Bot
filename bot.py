a72cbae8b98003dcf1b7129ef47871e1781fd24e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-28 22:03:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from pyrogram import Client
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "6043919342:AAGVl9ktA1YDKYvVKu-6xycVX7-9Fg69ZbU")
 
-API_ID = int(os.environ.get("API_ID",12345))
+API_ID = int(os.environ.get("API_ID",11973721))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
 
+FORCE_SUB   = os.environ.get("FORCE_SUB", "Star_Bots_Tamil") 
 
 if __name__ == "__main__" :
     plugins = dict(
