e3dc16bda60d278ffc7edc8c1d5a7a6dfa5af0b6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-28 22:50:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ import re, os, time
 from datetime import datetime
 from pytz import timezone
 from pyrogram.raw.all import layer
-from config import Config
 from aiohttp import web
 from route import web_server
+
+id_pattern = re.compile(r'^.\d+$') 
 
 BOT_TOKEN = os.environ.get("TOKEN", "6043919342:AAGVl9ktA1YDKYvVKu-6xycVX7-9Fg69ZbU")
 API_ID = int(os.environ.get("API_ID",11973721))
@@ -13,6 +14,7 @@ API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
 BOT_UPTIME  = time.time()
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1391556668').split()]
 
 class Bot(Client):
 
