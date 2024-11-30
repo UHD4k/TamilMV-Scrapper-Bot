0814ba60446d8e29abb173022d6a8cf6a8d30d18
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-11-30 21:59:10 +0530

    Update scraper.py

--- plugins/scraper.py
+++ plugins/scraper.py
@@ -8,7 +8,22 @@ async def tamilmv(url):
     soup = BeautifulSoup(resp.text, "html.parser")
     mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
     tor = soup.select('a[data-fileext="torrent"]')
-    
+    parse_data = f"<b><u>{soup.title.string}</u></b>"
+    for no, (t, m) in enumerate(zip(tor, mag), start=1):
+        filename = sub(r"www\S+|\- |\.torrent", "", t.string)
+        parse_data += f"""
+        
+{no}. <code>{filename}</code>
+┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>"""
+    return parse_data
+
+async def tamilmv1(url):
+    cget = create_scraper().request
+    resp = cget("GET", url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
+    tor = soup.select('a[data-fileext="torrent"]')
+
     # Full response data with both magnet and torrent links
     parse_data = f"<b><u>{soup.title.string}</u></b>"
 
@@ -16,12 +31,9 @@ async def tamilmv(url):
     
     for no, (t, m) in enumerate(zip(tor, mag), start=1):
         filename = sub(r"www\S+|\- |\.torrent", "", t.string)
-        parse_data += f"""
-        
-{no}. <code>{filename}</code>
-┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>"""
         
         # Add only torrent links to the list for the channel
         torrent_links.append(t['href'])
-    
-    return parse_data, torrent_links
+
+    # Return full data and the list of torrent links
+    return torrent_links
