5374894036a3095bd07f3d7923800fa1610e1366
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-01 05:42:01 +0530

    Update bypass_checker.py

--- plugins/core/bypass_checker.py
+++ plugins/core/bypass_checker.py
@@ -16,6 +16,39 @@ def is_excep_link(url):
         )
     )
 
+async def direct_link_checker(link, onlylink=False):
+    domain = urlparse(link).hostname
+
+    # Scraper
+    if bool(match(r"https?:\/\/.+\.1tamilmv\.\S+", link)):
+        return await tamilmv(link)
+    elif bool(match(r"https?:\/\/.+\.1tamilmv\.\S+", link)):
+        return await tamilmv1(link)
+
+    # Exceptions
+    elif bool(match(r"https?:\/\/.+\.technicalatg\.\S+", link)):
+        raise DDLException("Bypass Not Allowed!")
+    else:
+        raise DDLException(
+            f"<i>No Bypass Function Found for your Link:</i> <code>{link}</code>"
+        )
+
+    if onlylink:
+        return blink
+
+    links = []
+    while True:
+        try:
+            links.append(blink)
+            blink = await direct_link_checker(blink, onlylink=True)
+            if is_excep_link(links[-1]):
+                links.append("\n\n" + blink)
+                break
+
+        except Exception:
+            break
+    return links
+
 async def direct_link_checker1(link, onlylink=False):
     # Scraper logic
     domain = urlparse(link).hostname
