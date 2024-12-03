b56cf06aca90f1495456b830e91a5bbc3cbe2339
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-04 04:31:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,12 @@ from pytz import timezone
 from pyrogram.raw.all import layer
 from aiohttp import web
 from route import web_server
+import pyromod
+import pyrogram.utils
+from plugins.core.bypass_checker import app as Client2
+
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 id_pattern = re.compile(r'^.\d+$') 
 
@@ -15,6 +21,7 @@ BOT_UPTIME  = time.time()
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1391556668 1242556540').split()]
+STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKEBu5Pf_Oesjuxt4TIzNijt1iMjJ8hEa3xtURQFrsd0GFYLhS_XFm2iJ61NfFeKR5icfMSu_SWH3eRvvdZ-X7IyOVFZuQ4sHKoiju_WXCH4uQqqd7vB7_9hGyMbDk7mUgjVKNkRg0trupt-5mu8pAeWAZ3US61kBnLKvsMYSjiaiL3uWI3UDfzyNQzFhf_hXWF_XskD0QrMPS87wEd85iNzXBgBE9Sae2haJ8YppGWxhcGtmJDSqHnDSlxh2dFLBZ1K_o7zxE6i1FrOaqEL_gKW87xqc2W43kCsUj-s9A9GyXdP7aUxu1Mku5j3GyMxEWS79Yku7AfxyeGUYhTw5dXGScE=")
 
 class Bot(Client):
 
@@ -55,5 +62,13 @@ class Bot(Client):
         await super().stop()      
         print("Bot Stopped... Bye")
        
-
-Bot().run()
+if STRING_SESSION:
+    apps = [Client2, Bot]
+    for app in apps:
+        app.start()
+    idle()
+    for app in apps:
+        app.stop()
+    
+else:
+    Bot().run()
