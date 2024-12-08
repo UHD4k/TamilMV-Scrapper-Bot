c6a093553dd9158b6ee879074da404f7d430b6fa
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-08 13:32:50 +0530

    Update scraper.py

--- plugins/scraper.py
+++ plugins/scraper.py
@@ -1,21 +1,6 @@
 from cloudscraper import create_scraper
 from re import sub
 from bs4 import BeautifulSoup
-    
-async def tamilmv5(url):
-    cget = create_scraper().request
-    resp = cget("GET", url)
-    soup = BeautifulSoup(resp.text, "html.parser")
-    mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
-    tor = soup.select('a[data-fileext="torrent"]')
-    parse_data = f"<b><u>{soup.title.string}</u></b>"
-    for no, (t, m) in enumerate(zip(tor, mag), start=1):
-        filename = sub(r"^(www\.\S+\s-\s)|\.torrent$", "", t.string.strip())
-        parse_data += f"""
-        
-<b>{no}.</b> <code>{filename}</code>
-<b>┖ Links : <a href="https://t.me/share/url?url={m['href']}">Magnet 🧲</a>  | <a href="{t['href']}">Torrent 🌐</a></b>"""
-    return parse_data
 
 async def tamilmv(url):
     cget = create_scraper().request
