6682e6d2cbf9b21ba9cc91bf5faf46e17f8e8a17
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-08 14:28:35 +0530

    Update scraper.py

--- plugins/scraper.py
+++ plugins/scraper.py
@@ -50,13 +50,31 @@ async def tamilmv1(url):
     
     soup = BeautifulSoup(resp.text, "html.parser")
     tor = soup.select('a[data-fileext="torrent"]')
-
     torrent_links = []
-    
+    def clean_filename(raw_filename):
+        filename = sub(r"^(www\.\S+\s-\s)|\.torrent$", "", raw_filename.strip())
+        replacements = [
+            (r"\bAuds\b", "Audios"),
+            (r"\bAud\b", "Audio"),
+            (r"\bOrg\b", "Original"),
+            (r"\bTam\b", "Tamil"),
+            (r"\bTel\b", "Telugu"),
+            (r"\bHin\b", "Hindi"),
+            (r"\bEng\b", "English"),
+            (r"\bMal\b", "Malayalam"),
+            (r"\bKan\b", "Kannada"),
+            (r"\bKor\b", "Korean"),
+            (r"\bChi\b", "Chinese"),
+        ]
+        for pattern, replacement in replacements:
+            filename = sub(pattern, replacement, filename)
+        
+        return filename
+
     for t in tor:
         if t.string:
             torrent_link = t['href']
-            filename = sub(r"www\S+|\.torrent", "", t.string.strip())
+            filename = clean_filename(t.string)
             # Format the response as required
             formatted_response = f"<b>/qbleech {torrent_link}\nFile Name :-</b> </code>{filename}</code>", parse_mode=enums.ParseMode.HTML
             torrent_links.append(formatted_response)
