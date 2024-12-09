e613146514194dc0b38b973093ab1f42a0d084a8
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-09 11:08:05 +0530

    Update bypass.py

--- plugins/bypass.py
+++ plugins/bypass.py
@@ -5,8 +5,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from pyrogram.enums import MessageEntityType
 from pyrogram.errors import QueryIdInvalid
 import os, re
-from plugins.core.bypass_checker import direct_link_checker, direct_link_checker1, direct_link_checker2, is_excep_link, process_link_and_send, process_link_and_send1
-from plugins.core.bot_utils import convert_time, BypassFilter, BypassFilter1, BypassFilter2
+from plugins.core.bypass_checker import direct_link_checker, direct_link_checker1, direct_link_checker2, is_excep_link, process_link_and_send, process_link_and_send1, process_link_and_sendg
+from plugins.core.bot_utils import convert_time, BypassFilter, BypassFilter1, BypassFilter2, BypassFilterg
 from time import time
 
 # Configs
@@ -45,9 +45,9 @@ async def bypass(client, message):
     output = []
     for result, link in zip(results, links):
         if isinstance(result, Exception):
-            output.append(f"┖ <b>Error:</b> {result}")
+            output.append(f"┖ <b>Error :-</b> {result}")
         else:
-            output.append(f"┖ <b>Bypass Link:</b> {result}")
+            output.append(f"┖ <b>Bypass Link :-</b> {result}")
 
     elapsed = time() - start
     reply_text = "\n".join(output)
@@ -120,6 +120,39 @@ async def send_magnets(client, message):
     else:
         await message.reply("<b>No valid links found in the message!</b>")
 
+# Send Torrent Links to Group
+@Client.on_message(BypassFilterg & filters.user(ADMINS))
+async def send_torrents_group(client, message):
+    if (reply_to := message.reply_to_message) and (
+        reply_to.text or reply_to.caption
+    ):
+        txt = reply_to.text or reply_to.caption
+        entities = reply_to.entities or reply_to.caption_entities
+    elif AUTO_BYPASS or len(message.text.split()) > 1:
+        txt = message.text
+        entities = message.entities
+    else:
+        await message.reply("<b>No links provided!</b>")
+        return
+
+    links = []
+    tasks = []
+
+    for entity in entities:
+        if entity.type in (MessageEntityType.URL, MessageEntityType.TEXT_LINK):
+            link = txt[entity.offset:entity.offset + entity.length]
+            links.append(link)
+            tasks.append(create_task(process_link_and_sendg(client, link)))
+
+    if tasks:
+        results = await gather(*tasks, return_exceptions=True)
+        for result in results:
+            if isinstance(result, Exception):
+                print(f"Error during processing: {result}")
+        await message.reply("<b>Torrent Links Sent to Leech Group Successfully!</b>")
+    else:
+        await message.reply("<b>No valid links found in the message!</b>")
+
 # Inline query for bypass
 @Client.on_inline_query()
 async def inline_query(client, query):
@@ -132,7 +165,7 @@ async def inline_query(client, query):
         try:
             result = await direct_link_checker(link, True)
             elapsed = time() - start
-            response = f"┎ <b>Source Link :-</b> {link}\n\n┖ <b>Bypass Link:</b> {result}\n\n<b>Time: {convert_time(elapsed)}</b>"
+            response = f"┎ <b>Source Link :-</b> {link}\n\n┖ <b>Bypass Link :-</b> {result}\n\n<b>Time: {convert_time(elapsed)}</b>"
         except Exception as e:
             response = f"<b>Error:</b> {e}"
 
@@ -146,7 +179,7 @@ async def inline_query(client, query):
         # Provide help text if the query is empty
         help_text = """<b><i>1TamilMV Scrapper Bot</i></b>
         
-<b>Use the inline query format: [link]</b>
+<b>Use the inline query format :- [link]</b>
 """
         answer = InlineQueryResultArticle(
             title="Bypass Help",
