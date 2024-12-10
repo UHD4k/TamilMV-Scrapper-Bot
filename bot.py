8646f2753d4522b42189b61a9a8f1a12598a9d67
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-10 17:03:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from pyrogram.raw.all import layer
 from aiohttp import web
 from route import web_server
 import pyromod
-import time
 from plugins.core.rss_feed import tamilmv_rss_feed, tamilblasters_rss_feed, TAMILBLAST_LOG, TAMILMV_LOG
 import pyrogram.utils
 from plugins.core.bypass_checker import app as Client2
@@ -17,9 +16,9 @@ pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 id_pattern = re.compile(r'^.\d+$') 
 
 BOT_TOKEN = os.environ.get("TOKEN", "7713324413:AAGYzSbK5Z9KbhaJGiQdA7Rs-YaYLk4fNiU")
-API_ID = int(os.environ.get("API_ID",11973721))
+API_ID = int(os.environ.get("API_ID", 11973721))
 API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
-BOT_UPTIME  = time.time()
+BOT_UPTIME = time.time()
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1391556668 1242556540').split()]
@@ -33,36 +32,41 @@ Bot = Client(
     plugins=dict(root="plugins"),
 )
 
-async def start_bot(client: Client):
-    """Start the bot and run the RSS scraping tasks."""
-    await client.start()
-    print("Bot Started!")
-    for chat in [TAMILMV_LOG, TAMILBLAST_LOG]:
-        await client.send_message(chat, "**Bot Started!**")
+async def start_bot(clients):
+    """Start the bots and run the RSS scraping tasks."""
+    for client in clients:
+        await client.start()
+        print(f"{client.name} Started!")
+        for chat in [TAMILMV_LOG, TAMILBLAST_LOG]:
+            await client.send_message(chat, "**Bot Started!**")
     while True:
         print("TamilMV RSS Feed Running...")
-        await tamilmv_rss_feed(client)
+        await tamilmv_rss_feed(Bot)
         time.sleep(150)
 
         print("TamilBlasters RSS Feed Running...")
-        await tamilblasters_rss_feed(client)
+        await tamilblasters_rss_feed(Bot)
         time.sleep(300)
 
-async def stop_bot(client: Client):
-    """Stop the bot."""
-    await client.stop()
-    print("Bot Stopped!")
+async def stop_bot(clients):
+    """Stop the bots."""
+    for client in clients:
+        await client.stop()
+        print(f"{client.name} Stopped!")
 
 if STRING_SESSION:
-    # Include both the default bot and RSS scraping logic
+    # Include both Bot and Client2
     async def main():
+        clients = [Bot, Client2]
         try:
-            await start_bot(Bot)
+            await start_bot(clients)
         except KeyboardInterrupt:
-            await stop_bot(Bot)
+            await stop_bot(clients)
     Bot.start()
+    Client2.start()
     idle()
     Bot.stop()
+    Client2.stop()
 else:
     Bot.run()
     
