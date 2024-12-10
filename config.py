1eab5185f69f0a9d63aa141fb2bd27cbec176996
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-10 10:14:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,10 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 class Config:
+    API_ID = int(os.environ.get("API_ID", "11329245"))
+    API_HASH = os.environ.get("API_HASH", "90988dcdd5ddd4d0a28843a1e2605924")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7713324413:AAGYzSbK5Z9KbhaJGiQdA7Rs-YaYLk4fNiU")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKEBu5Pf_Oesjuxt4TIzNijt1iMjJ8hEa3xtURQFrsd0GFYLhS_XFm2iJ61NfFeKR5icfMSu_SWH3eRvvdZ-X7IyOVFZuQ4sHKoiju_WXCH4uQqqd7vB7_9hGyMbDk7mUgjVKNkRg0trupt-5mu8pAeWAZ3US61kBnLKvsMYSjiaiL3uWI3UDfzyNQzFhf_hXWF_XskD0QrMPS87wEd85iNzXBgBE9Sae2haJ8YppGWxhcGtmJDSqHnDSlxh2dFLBZ1K_o7zxE6i1FrOaqEL_gKW87xqc2W43kCsUj-s9A9GyXdP7aUxu1Mku5j3GyMxEWS79Yku7AfxyeGUYhTw5dXGScE=")
     OWNER_ID = int(os.environ.get("OWNER_ID", 1391556668))
     GROUP_ID = int(os.environ.get("GROUP_ID", -1001542301808))
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority")
