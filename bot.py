8ae5ed66bc5a840df837963cccc6065320cda41b
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-10 11:56:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,63 +1,37 @@
-# Copyright 2024-present, Author: MrTamilKiD
+from pyrogram import Client, __version__, idle
+import re, os, time
+from datetime import datetime
+from pytz import timezone
+from pyrogram.raw.all import layer
+from aiohttp import web
+from route import web_server
+import pyromod
+import pyrogram.utils
+from plugins.core.bypass_checker import app as Client2
 
-import time
-from typing import Union
-from pyrogram import Client as pyClient
-from pyrogram.storage import Storage
-from config import Config, LOGGER
-from plugins.core.bypass_checker import tamilmv_rss_feed, tamilblasters_rss_feed, tamilmv_rss_feed_user, tamilblasters_rss_feed_user
-from plugins.core.bypass_checker import app as Client2  # Import Client2
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
-class Client(pyClient):
-    """ Custom Bot Class """
+id_pattern = re.compile(r'^.\d+$') 
 
-    def __init__(self, session_name: Union[str, Storage] = "TamilMVAutoRss-Bot"):
-        super().__init__(
-            session_name,
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            plugins=dict(
-                root="MrTamilKiD/plugins"
-            )
-        )
+BOT_TOKEN = os.environ.get("TOKEN", "7713324413:AAGYzSbK5Z9KbhaJGiQdA7Rs-YaYLk4fNiU")
+API_ID = int(os.environ.get("API_ID",11973721))
+API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
+BOT_UPTIME  = time.time()
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1391556668 1242556540').split()]
+STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKEBu5Pf_Oesjuxt4TIzNijt1iMjJ8hEa3xtURQFrsd0GFYLhS_XFm2iJ61NfFeKR5icfMSu_SWH3eRvvdZ-X7IyOVFZuQ4sHKoiju_WXCH4uQqqd7vB7_9hGyMbDk7mUgjVKNkRg0trupt-5mu8pAeWAZ3US61kBnLKvsMYSjiaiL3uWI3UDfzyNQzFhf_hXWF_XskD0QrMPS87wEd85iNzXBgBE9Sae2haJ8YppGWxhcGtmJDSqHnDSlxh2dFLBZ1K_o7zxE6i1FrOaqEL_gKW87xqc2W43kCsUj-s9A9GyXdP7aUxu1Mku5j3GyMxEWS79Yku7AfxyeGUYhTw5dXGScE=")
 
-    async def start(self):
-        await super().start()
-        LOGGER.info("Bot Started!")
-        
-        # Notify in log channels
-        for chat in Config.TAMILMV_LOG, Config.TAMILBLAST_LOG, Config.GROUP_ID:
-            await self.send_message(chat, "Bot Started!")
-        
-        # Start Client2
-        await Client2.start()
-        
-        # Send message to group_id using Client2
-        await Client2.send_message(Config.GROUP_ID, "Bot Started For Auto Leech")
+Bot = Client("1TamilMVScrapper", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
 
-        # Start the scraper loop
-        while True:
-            print("TamilMV RSS Feed Running...")
-            await tamilmv_rss_feed(self)
-
-            print("TamilBlasters RSS Feed Running...")
-            await tamilblasters_rss_feed(self)
-
-            # Send message to GROUP_ID using Client2
-            print("TamilMV RSS Feed Sending update to group...")
-            await tamilmv_rss_feed_user(Client2)
-            
-            print("Tamilblasters RSS Feed Sending update to group...")
-            await tamilblasters_rss_feed_user(Client2)
-
-            print("Sleeping for 5 minutes...")
-            time.sleep(300)
-
-    async def stop(self, *args):
-        await super().stop()
-        LOGGER.info("Bot Stopped!")
-
-        # Stop Client2
-        await Client2.stop()
-        
+if STRING_SESSION:
+    apps = [Client2,Bot]
+    for app in apps:
+        app.start()
+    idle()
+    for app in apps:
+        app.stop()
+    
+else:
+    Bot().run()
