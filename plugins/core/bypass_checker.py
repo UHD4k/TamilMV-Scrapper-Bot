deab34b3d517c1dc3bbb97ede1f73a08ba1a9fda
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-10 07:16:41 +0530

    Update bypass_checker.py

--- plugins/core/bypass_checker.py
+++ plugins/core/bypass_checker.py
@@ -554,3 +554,94 @@ async def tamilrockers_rss_feed(bot: Client):
             except Exception as e:
                 print(e)
                 pass
+
+
+
+
+
+
+
+
+
+# send message to user 
+
+# tamilblasters rss feed function
+
+async def tamilblasters(bot: Client):
+    tamilblasters_url = await u_db.get_domain("1TamilBlasters")
+    if not tamilblasters_url:
+        print("Error: TamilBlasters domain not found in the database.")
+        return
+    main_link = []
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'Connection': 'Keep-alive',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+
+    global movie_dict
+    movie_dict = {}
+    global real_dict
+    real_dict = {}
+    web = requests.request("GET", tamilblasters_url, headers=headers)
+    soup = BeautifulSoup(web.text, 'lxml')
+    linker = []
+    bad_titles = []
+    real_titles = []
+    global movie_list
+    movie_list = []
+
+    num = 0
+
+    temps = soup.find_all('div', {'class': 'ipsType_break ipsContained'})
+
+    for i in range(41):
+        title = temps[i].findAll('a')[0].text
+        bad_titles.append(title)
+        links = temps[i].find('a')['href']
+        content = str(links)
+        linker.append(content)
+
+    for element in bad_titles:
+        real_titles.append(element.strip())
+        movie_dict[element.strip()] = None
+
+    movie_list = list(movie_dict)
+
+    for url in linker:
+        html = requests.request("GET", url, headers=headers)
+        soup = BeautifulSoup(html.text, 'lxml')
+        pattern = re.compile(r"magnet:\?xt=urn:[a-z0-9]+:[a-zA-Z0-9]{40}")
+        big_title = soup.find_all('a')
+        all_titles = []
+        file_link = []
+        mag = []
+        for i in soup.find_all('a', href=True):
+            if i['href'].startswith('magnet'):
+                mag.append(i['href'])
+
+        for a in soup.findAll('a', {"data-fileext": "torrent", 'href': True}):
+            file_link.append(a['href'])
+            all_titles.append(re.sub(r'www\.[a-z0-9.\-]+', '', a.text, flags=re.IGNORECASE).replace('.torrent', '').replace('-', '', 1).replace(' ', '', 2))
+
+        for p in range(0, len(mag)):
+            try:
+                real_dict.setdefault(movie_list[num], [])
+                real_dict[movie_list[num]].append((f"/ql {file_link[p]} \n\n **{all_titles[p]}**"))
+                if not await u_db.is_tb_exist(all_titles[p], file_link[p], mag[p]):
+                    await bot.send_message(chat_id=Config.TAMILBLAST_LOG,
+                        text=f"<b>/qbleech {file_link[p]}\n\nFile Name :- {all_titles[p]}</b>\n\n<b>📥 Updated By <a href='https://t.me/DP_BOTZ'>1TamilBlasters</a></b>", disable_web_page_preview=True)
+                    await app.send_message(chat_id=Config.GROUP_ID,
+                        text=f"<b>/qbleech {file_link[p]}\n\nFile Name :- {all_titles[p]}</b>", disable_web_page_preview=True)
+                    print(f"added working...")
+                    await u_db.add_tb(all_titles[p], file_link[p], mag[p])
+                    await asyncio.sleep(3)
+            except Exception as e:
+                print(e)
+                pass
+
+        num = num + 1
+    return real_dict
